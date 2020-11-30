f2534c57dd60912d13ff39e8f00928dfe93f3f9f
siebtel <siebtel.ss@gmail.com>
2020-11-30 11:08:27 -0300

    english translation

---
+++ proc_generator/en.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+english = {
+  "article_detail":{
+    "article_files": "Article file",
+    "article_pdf": "Article PDF",
+    "presentation_pdf": "PDF presentation available soon",
+    "authors": "Authors",
+    "affiliations": "Affiliations",
+    "theme": "Theme",
+    "session": "Session",
+    "abstract": "Abstract"
+  },
+  "article_list":{
+    "list_contributions": "Contributions List"
+  },
+  "article_macros":{
+    "theme": "Theme",
+    "session": "Session"
+  },
+  "author_list":{ 
+    "list_authors": "Authors List"
+  },
+  "base":{
+    "conference_proceedings": "Conference Proceedings",
+    "mise": "Web Design : "
+  },
+  "chair_eval_index":{ 
+    "conference_proceedings": "Conference Proceedings",
+    "ficheI": "Session Evaluation Files",
+    "documents": "Documents to be distributed to the chairmen of each session for the evaluation of the sessions and presentations.",
+    "pour": 'For each session you have to fill out : ',
+    "a_session_evaluation_sheet": "Session Evaluation Sheet",
+    "the_presentation_evaluation_sheets": "Presentation Evaluation Sheet (one per contribution)",
+    "ensemble": "1) All Files",
+    "fichesIV": 'Concatenated files in a folder, sorted by session : ',
+    "telecharge": "Download the",
+    "pdf": "PDF Files",
+    "voir": "See the",
+    "html": "HTML Files",
+    "number_of_session_files": 'Amount of session files',
+    "number_of_article_files": 'Amount of article files',
+    "separees": "2) Files separated by session",
+    "clique": "Click on the session to download the corresponding PDF file",
+    "president": "Président(e)",
+    "mise": 'Web Design : '
+  },
+  "chair_eval_presentation":{
+    "eval": "Presentation Evaluations",
+    "contributions": "All complete contributions evaluation with a view to their pre-selection for publication in partner journals.",
+    "session_chair": "Session Chair",
+    "verify_your_name": "Verify your name (correct it as needed).",
+    "titre": "Title",
+    "authors": "Authors",
+    "presentee": 'Presented by : ',
+    "presentation_article": "Your evaluation of the article and the presentations",
+    "contribution": 'Scientifique content of complete contribution : ',
+    "faible": "Poor",
+    "average": "Average",
+    "good": "Good",
+    "excellent": "Excellent",
+    "original": "'Presented work's originality :",
+    "qualite": 'Quality of the oral or poster presentation : ',
+    "echanges": 'Quality of interaction :',
+    "note": 'Overall rating of the contribution :',
+    "publication": "Publication in a partner journal",
+    "concernant": 'Concerning the pre-selection by the CS SBQS of the contribution for publication in a partner international journal, are you  : ',
+    "défavorable": "Disadvantageous",
+    "plutôt_def": "Mostly Disadvantageous",
+    "plutôt_fav": "Mostly advantageous",
+    "favorable": "Advantageous",
+    "nb": 'Nota Bene : If the paper is pre-selected, a review will be carried out according to the rules of the partner journals (EJEE, EPE, EJAP). This review will be carried out by an ad-hoc committee on the basis of the complete English language version of the EMS article.',
+    "optional_comments": 'Optional comments :',
+  },
+  "chair_eval_session": {
+    "session_summary": "Session Summary",
+    "chair_documents": "Documents to the Chair",
+    "chair_documents_description": "Documents to be distributed to the chairmen of each session for the evaluation of the sessions and presentations.",
+    "fill_out_for_each_session": 'For each session you have to fill out : ',
+    "a_session_evaluation_sheet": "Session Evaluation Sheet",
+    "the_presentation_evaluation_sheets": "Presentation Evaluation Sheet (one per contribution)",
+    "files_classified_by_session": "Files classified by session",
+    "number_of_session_files": 'Amount of session files',
+    "number_of_article_files": 'Amount of article files',
+    "session_chair": "Session Chair",
+    "verify_your_name": "Verify your name (correct it as needed).",
+    "number_of_presentations_carried_out": 'Amount of presentations carried out : ',
+    "your_session_evaluation": "Your session evaluation",
+    "approximate_number_attendees": 'Approximate number of attendees : ',
+    "global_presentation_quality": 'Global quality of the presentations : ',
+    "interaction_discussion_quality": 'Quality of interaction and discussion : ',
+    "session_duration": 'Session duration : ',
+    "too_short": "Too short",
+    "appropriate": "Appropriate",
+    "too_long": "Too long",
+    "physical_conditions": 'The material conditions are : ',
+    "faible": "Insufficient",
+    "poor": "Poor",
+    "average": "Average",
+    "good": "Good",
+    "excellent": "Excellent",
+    "optional_comments": 'Optional comments : ',
+    'comite': "The SBQS organizing committee would like to thank you very much for your contribution and expertise, which were essential to the symposium..",
+    "session": "Session",
+    "article": "article"
+  }, 
+  "index":{
+    "conference_proceedings": "Conference Proceedings",
+    "list_all_contributions": "Contributions List",
+    "session_program": "Session Program",
+    "the_authors": "Authors",
+    "website": 'Conference Website :',
+    "theme": "Theme",
+    "sponsors": 'Sponsors',
+  },
+  "lab_list":{
+    "labs_list": "List of Laboratories",
+    "labs": "Laboratories and Research Centers",
+    "list_of_contributions": 'List of contributions classified by: laboratories, research centers or institutions of the conference contributors : '
+  },
+  "program_book":{
+    "program": "Program",
+    "program_detail": "Program details",
+    "room": "Room",
+    "president": "President",
+    "index": "Authors index",
+    "references": "Referenced Author"
+  },
+  "session_detail":{
+    "session_title": "Session",
+    "lsession": 'session',
+    "president": "President",
+    "articles": "Session's articles :",
+    "return": "Return to session's list"
+  },
+  "session_list":{
+    "session_program": "Session's Program"
+  },
+  "topic_detail":{
+    "topic": "Topic",
+    "theme": "Theme",
+    "articles": 'Articles in this theme :'
+  }
+}
